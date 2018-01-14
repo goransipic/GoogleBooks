e11b62df0e19141f120749da8c448bb64b16654e
gsipic <Goran.Sipic@asseco-see.hr>
2018-01-15 00:06:51 +0100

    added account book api key

--- app/src/main/java/com/goodapp/googlebooks/di/AppModule.java
+++ app/src/main/java/com/goodapp/googlebooks/di/AppModule.java
@@ -2,13 +2,22 @@ package com.goodapp.googlebooks.di;
 
 import android.app.Application;
 
+import com.goodapp.googlebooks.BuildConfig;
 import com.goodapp.googlebooks.api.GoogleApiBooks;
 import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+
+import java.io.IOException;
 
 import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
+import okhttp3.HttpUrl;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -18,13 +27,44 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 @Module(includes = ViewModelModule.class)
 class AppModule {
-    @Singleton @Provides
-    GoogleApiBooks provideGoogleBooksService() {
+
+    @Singleton
+    @Provides
+    GoogleApiBooks provideGoogleBooksService(OkHttpClient client) {
         return new Retrofit.Builder()
                 .baseUrl("https://www.googleapis.com/books/v1/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .client(client)
                 .build()
                 .create(GoogleApiBooks.class);
     }
+
+    @Provides
+    OkHttpClient provideHttpClient() {
+
+        HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
+        httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+        httpClient.addInterceptor(chain -> {
+            Request original = chain.request();
+
+            HttpUrl originalHttpUrl = original.url();
+            HttpUrl url = originalHttpUrl.newBuilder()
+                    .addQueryParameter("key", "AIzaSyDnetujB0jerBIiviykhHL_1OVI37aNuug")
+                    .build();
+
+            Request.Builder requestBuilder = original.newBuilder()
+                    .url(url);
+
+            Request request = requestBuilder.build();
+
+            return chain.proceed(request);
+        }).addInterceptor(httpLoggingInterceptor);
+
+        return httpClient.build();
+
+    }
+
 }
